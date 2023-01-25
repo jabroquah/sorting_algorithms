ac59379b1de79d4d8eec20d69c4ce4d5dbb996ad
Joseph Abroquah <abroquahjoseph@gmail.com>
2023-01-25 23:33:37 +0000

    Initial commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,22 @@
+void bubble_sort(int *arrary, size_t size)
+{
+	int temp;
+
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				for (int k = 0; k < size; k++)
+				{
+					print("%d ", array[k]);
+				}
+				printf("\n");
+			}
+		}
+	}
+}
