96cf60a7b6f59b3ca01a6a3117e4f7634e551b0b
Joseph Abroquah <abroquahjoseph@gmail.com>
2023-01-26 04:21:45 +0000

    Updated

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,25 +6,16 @@
 *
 */
 
-void bubble_sort(int *array, size_t size)
-{
-	int temp;
-
-	for (int i = 0; i < size - 1; i++)
-	{
-		for (int j = 0; j < size - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				for (int k = 0; k < size; k++)
-				{
-					printf("%d ", array[k]);
-				}
-				printf("\n");
-			}
-		}
-	}
+void bubble_sort(int array[], size_t size) {
+    size_t i, j;
+    for (i = 0; i < size - 1; i++) {
+        for (j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+            }
+        }
+    }
 }
